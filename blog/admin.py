917764028f806936195cde431e26d3d345e9a90b
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-06-26 19:36:46 -0300

    Revert "django-admin"

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    search_fields = ('title', 'content')
-    list_filter = ('created_at',)
-
