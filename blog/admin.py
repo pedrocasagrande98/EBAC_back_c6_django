bad419aae3ecf37d1309fd85ccbf525d1af579ab
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-06-26 19:07:31 -0300

    'add modo admin'

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at',)
+
