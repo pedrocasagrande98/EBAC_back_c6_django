a6623e558fd090d3069852cebb2a9c8611cb6c37
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-06-26 22:33:17 -0300

    Seguindo templates propostos em aula

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    search_fields = ('title', 'content')
-    list_filter = ('created_at',)
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    list_filter = ("status",)
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
