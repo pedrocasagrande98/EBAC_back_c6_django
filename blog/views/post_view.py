a73b1d5b83058bdb5e0373bf193dd3cdb6edc6a2
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-06-26 23:01:18 -0300

    add aula4 -nova estrutura de views do exercício

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -2,7 +2,7 @@ from django.views import generic
 from .models import Post
 
 
-class PostList(generic.ListView):
+class PostView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
 
