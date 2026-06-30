a80025b7e9464890e7fb8bdccb328377fd69a163
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-06-29 22:27:42 -0300

    'add Slug + Rascunho/Publicado'

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title', 'status', 'created_at')
     search_fields = ('title', 'content')
-    list_filter = ('created_at',)
-
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
